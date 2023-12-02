762af7e5d55c224a208e1e1ddd14e51377e0e080
Leander <leanderfdo12@gmail.com>
2023-12-02 12:48:38 +0530

    Fix role name and remote state names

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tfstate"
+  bucket = "tfstate-zivvy"
 
   lifecycle {
     prevent_destroy = true
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "app-state"
+  name           = "app-state-zivvy"
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "LockID"
