54b27e113c0ca0668b6a14766cb278c2c8bb9e63
Leander <leanderfdo12@gmail.com>
2023-12-03 00:06:43 +0530

    ECR image retention policy

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -6,3 +6,27 @@ resource "aws_ecr_repository" "zivvy" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "foopolicy" {
+  repository = aws_ecr_repository.zivvy.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 5 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
