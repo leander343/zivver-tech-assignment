9b887b3b5d4554bab4059c27f5e5efd9e47353a5
leander343 <72292880+leander343@users.noreply.github.com>
2023-12-02 13:02:33 +0530

    Update terraform.yml
    Update terraform.yml
    
    Update terraform.yml

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "zivvy" {
-  name                 = "zivvy"
+  name                 = "zivvy-ecr"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
